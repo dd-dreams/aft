346d3b02ad026c327e6f09de92d8145492265de8
dd-dreams <80887265+dd-dreams@users.noreply.github.com>
2023-10-17 18:43:39 +0300

    fix: build

--- aft/src/sender.rs
+++ aft/src/sender.rs
@@ -89,7 +89,7 @@ where
     /// Constructs a new Sender struct, and connects to `remote_ip`.
     pub fn new(remote_addr: &str, ident: String, encryptor_func: fn(&[u8]) -> T) -> Self {
         // Remove http(s):// since aft doesn't support HTTPS.
-        let no_http_addr = remote_ip.replace("http://", "").replace("https://", "");
+        let no_http_addr = remote_addr.replace("http://", "").replace("https://", "");
         let socket = TcpStream::connect(no_http_addr.to_socket_addrs().expect("Couldn't resolve IP").next()
             .expect("IP Not resolved"))
             .expect("Couldn't connect.");
